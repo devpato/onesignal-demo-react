367f70dfa852c9ecd12d78db4fff4354a752974e
devpato <pevargas2730@gmail.com>
2021-01-20 00:51:40 -0500

    feat(data): created dummy data

---
+++ src/Data/data.js
@@ -0,0 +1,28 @@
+const categories = [
+    {
+        label: "VueJS",
+        color: "green",
+        tag: "vuejs",
+        url: "https://youtube.com/embed/Wy9q22isx3U"
+    },
+    {
+        label: "ReactJS",
+        color: "blue",
+        tag: "reactjs",
+        url: "https://youtube.com/embed/sBws8MSXN7A"
+    },
+    {
+        label: "Angular",
+        color: "red",
+        tag: "angular",
+        url: "https://youtube.com/embed/Fdf5aTYRW0E"
+    },
+    {
+        label: "JavaScript",
+        color: "yellow",
+        tag: "javascript",
+        url: "https://www.youtube.com/embed/hdI2bqOjy3c"
+    }
+];
+
+export default categories;
