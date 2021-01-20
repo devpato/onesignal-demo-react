2591d04ea7cf997b80e2c723b011bff3f5368033
devpato <pevargas2730@gmail.com>
2021-01-19 23:33:48 -0500

    feat(setup): basic demo working for onesignal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  window.OneSignal = window.OneSignal || [];
+  const OneSignal = window.OneSignal;
+  useEffect(() => {
+    OneSignal.push(function () {
+      OneSignal.init(
+        {
+          appId: "1a9bbed2-9294-4192-a736-01b461cb389a",
+          promptOptions: {
+            slidedown: {
+              enabled: true,
+              actionMessage: "We'd like to show you notifications for the latest news and updates.",
+              acceptButtonText: "OMG YEEEEESS!",
+              cancelButtonText: "NAHHH",
+              categories: {
+                  tags: [
+                      {
+                          tag: "New Version",
+                          label: "New App Version",
+                      },
+                      {
+                        tag: "SDK Updates",
+                        label: "SDK Updates",
+                    }
+                  ]
+              }     
+          } 
+        },
+        welcomeNotification: {
+          "title": "One Signal",
+          "message": "Thanks for subscribing!",
+        } 
+      });
+      OneSignal.setEmail("devpatovg@gmail.com");
+    });
+  }, []);
+
+
+   const onSubscribe = () => {
+    OneSignal.sendTags({ category: 'ReactJS' });
+    alert('Thanks for subscribing to ReactJS')
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={onSubscribe}>Subscribe to REACT JS</button>
     </div>
   );
 }
