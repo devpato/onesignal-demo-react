2d2ee57987dd864a8e91657fdb44594411e6634c
devpato <pevargas2730@gmail.com>
2021-01-23 02:59:01 -0500

    fix(init): cleanup code for initial state

--- src/App.js
+++ src/App.js
@@ -5,48 +5,18 @@ import categories from './Data/data';
 
 
 function App() {
-  window.OneSignal = window.OneSignal || [];
-  const OneSignal = window.OneSignal;
+  
   useEffect(() => {
-    OneSignal.push(function () {
-      OneSignal.init(
-        {
-          appId: "1a9bbed2-9294-4192-a736-01b461cb389a",
-          promptOptions: {
-            slidedown: {
-              enabled: true,
-              actionMessage: "We'd like to show you notifications for the latest news and updates.",
-              acceptButtonText: "OMG YEEEEESS!",
-              cancelButtonText: "NAHHH",
-              categories: {
-                  tags: [
-                      {
-                          tag: "New Version",
-                          label: "New App Version",
-                      },
-                      {
-                        tag: "SDK Updates",
-                        label: "SDK Updates",
-                    }
-                  ]
-              }     
-          } 
-        },
-        welcomeNotification: {
-          "title": "One Signal",
-          "message": "Thanks for subscribing!",
-        } 
-      });
-      // OneSignal.setEmail("devpatovg@gmail.com");
-    });
+    /**
+     * YOUR ONE SIGNAL CONFIGURATION GOES HERE
+     */
   }, []);
 
 
    const subscriptionHandler = (tag) => {
-    OneSignal.push(()=> {
-      console.log('sendTag', tag)
-      OneSignal.sendTags({ category: tag });
-    });
+    /**
+     * HERE WE ARE GOING TO SUBSCRIBE TO THE SELECTED CATEGORY (TAG)
+     */
    }
 
   return (
